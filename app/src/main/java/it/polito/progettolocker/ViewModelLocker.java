a30b6f63477ca353606cf69df1e1f5fe73a79a76
LeslieLxt <lucadema22@gmail.com>
2023-12-11 11:49:09 +0100

    todo added

---
+++ app/src/main/java/it/polito/progettolocker/ViewModelLocker.java
@@ -0,0 +1,28 @@
+package it.polito.progettolocker;
+
+public class ViewModelLocker {
+
+
+    //TODO: collegamento a Firebase
+
+    //TODO: lista di articoli
+    // ARTICOLO: nome, prezzo, (immagine), quantità (3)
+
+    //TODO: carrello
+
+    //TODO: locker
+    // nome, indirizzo, lista cassetti
+    // TODO: cassetto
+    // stato (aperto/chiuso)
+    // disponibilità (libero/occupato)
+
+    //TODO: spedizioni
+    // ID spedizione, ID cliente, stato (in corso/ consegnato / conclusa)
+
+    //TODO: utente
+    // ID utente (firebase), ruolo, lista spedizioni (con storico),
+
+    //TODO: fattorino
+    // ID fattorino, ruolo, lista spedizioni, orario inizio (presa in carico), orario fine (consegna)
+
+}
