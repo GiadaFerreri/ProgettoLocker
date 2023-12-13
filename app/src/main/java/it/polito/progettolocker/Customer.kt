347831f4e0d1ebbb405dcc123ff2a2254b6dccb5
carolinaicardi1 <s315064@studenti.polito.it>
2023-12-13 14:39:12 +0100

    Inizio navigation

---
+++ app/src/main/java/it/polito/progettolocker/Customer.kt
@@ -0,0 +1,10 @@
+package it.polito.progettolocker
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun Customer(mainActivity: MainActivity, navController: NavController){
+    Text(text = "Customer")
+}
--- app/src/main/java/it/polito/progettolocker/MainActivity.kt
+++ app/src/main/java/it/polito/progettolocker/MainActivity.kt
@@ -3,13 +3,24 @@ package it.polito.progettolocker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import it.polito.progettolocker.ui.theme.ProgettoLockerTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +33,39 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                 Navigation(mainActivity = this)
                 }
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ProgettoLockerTheme {
-        Greeting("Android")
+fun Navigation(mainActivity: MainActivity){
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "HomePage"){
+        composable("HomePage"){
+            HomePage(mainActivity, navController)
+        }
+        composable("Delivery"){
+            Delivery(mainActivity, navController)
+        }
+        composable("Customer"){
+            Customer(mainActivity, navController)
+        }
     }
 }
+
+@Composable
+fun HomePage( mainActivity: MainActivity, navController: NavController) {
+    Row {
+        Button(onClick = {navController.navigate("Delivery")}, modifier = Modifier) {
+            Text(text = "Delivery")
+        }
+        Button(onClick = {navController.navigate("Customer", )}, modifier = Modifier) {
+        Text(text = "Customer")
+    }
+    }
+
+
+}
+
