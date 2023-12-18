5e5ba889fad769450bbaa0577e93bc289692d6b0
LeslieLxt <lucadema22@gmail.com>
2023-12-18 11:38:47 +0100

    prove firebase

--- app/src/main/java/it/polito/progettolocker/ViewModelLocker.kt
+++ app/src/main/java/it/polito/progettolocker/ViewModelLocker.kt
@@ -1,16 +1,32 @@
 package it.polito.progettolocker
 
+import android.content.ContentValues
+import android.util.Log
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.IgnoreExtraProperties
+import com.google.firebase.database.database
+import com.google.firebase.database.getValue
+import com.google.firebase.database.ValueEventListener
 import it.polito.progettolocker.dataClass.Article
 import it.polito.progettolocker.dataClass.Compartment
+import it.polito.progettolocker.dataClass.DeliveryMan
 import it.polito.progettolocker.dataClass.Locker
 import it.polito.progettolocker.dataClass.Shipping
+import it.polito.progettolocker.dataClass.States
+import it.polito.progettolocker.dataClass.User
+
 
 class ViewModelLocker : ViewModel() {
-
-    //TODO: collegamento a Firebase
 
     private val _articles = MutableLiveData<List<Article>>(emptyList())
     val articles : LiveData<List<Article>> = _articles
@@ -77,10 +93,12 @@ class ViewModelLocker : ViewModel() {
 
     fun reserveCompartment(compartment: Compartment){
         compartment.busy = true
+        //TODO: controllo
     }
 
     fun freeCompartment (compartment: Compartment) {
         compartment.busy = false
+        //TODO: controllo
     }
 
     //TODO: spedizioni
@@ -93,15 +111,21 @@ class ViewModelLocker : ViewModel() {
 
         //UPDATES
     fun startShipping(shipping: Shipping,deliverymanId: String){
-
+        if(shipping.deliverymanId == deliverymanId) {
+            shipping.state = States.HANDLED
+        }
     }
 
     fun deliveredShipping(shipping: Shipping,deliverymanId: String){
-
+        if(shipping.deliverymanId == deliverymanId) {
+            shipping.state = States.DELIVERED
+        }
     }
 
-    fun endShipping(shipping: Shipping,deliverymanId: String){
-
+    fun endShipping(shipping: Shipping,userId: String){
+        if(shipping.userId == userId) {
+            shipping.state = States.CONCLUDED
+        }
     }
 
     //TODO: utente
@@ -110,4 +134,37 @@ class ViewModelLocker : ViewModel() {
 
     //TODO: fattorino
     // ID fattorino, ruolo, lista spedizioni, orario inizio (presa in carico), orario fine (consegna)
+
+    //FUNZIONI FIREBASE (PROVE)
+    fun WriteInDatabase(databaseReference: DatabaseReference) {
+        databaseReference.child("Stringa").setValue("Hello, World!")
+    }
+
+    fun WriteBoolInDatabase(databaseReference: DatabaseReference) {
+        databaseReference.child("Bool").setValue(true)
+    }
+
+    fun WriteIntInDatabase(databaseReference: DatabaseReference) {
+        databaseReference.child("Int").setValue(1)
+    }
+
+    @Composable
+    fun ReadFromDatabase( modifier: Modifier = Modifier, databaseReference: DatabaseReference) {
+        // Read from the database
+
+        databaseReference.addValueEventListener(object: ValueEventListener {
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = snapshot.getValue<String>()
+                Log.d(ContentValues.TAG, "Value is: " + value)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.w(ContentValues.TAG, "Failed to read value.", error.toException())
+            }
+
+        })
+    }
 }
---
+++ app/src/main/java/it/polito/progettolocker/dataClass/DeliveryMan.kt
@@ -0,0 +1,5 @@
+package it.polito.progettolocker.dataClass
+
+data class DeliveryMan(
+    val idDeliveryMan: String
+)
