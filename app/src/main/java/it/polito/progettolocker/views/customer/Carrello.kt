9c315f5df1b18c75efcb3fe82a1895a44b58239b
Giuseppe <peppe.bruno99@yahoo.it>
2024-02-19 19:32:09 +0100

    gestione carrello vuoto

--- app/src/main/java/it/polito/progettolocker/views/customer/Carrello.kt
+++ app/src/main/java/it/polito/progettolocker/views/customer/Carrello.kt
@@ -3,9 +3,11 @@ package it.polito.progettolocker.views.customer
 import android.content.ContentValues
 import android.util.Log
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -13,12 +15,16 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableDoubleStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -26,6 +32,7 @@ import com.google.firebase.database.ValueEventListener
 import it.polito.progettolocker.MainActivity
 import it.polito.progettolocker.dataClass.Article
 import it.polito.progettolocker.dataClass.DataState
+import it.polito.progettolocker.graphic.Buttons
 import it.polito.progettolocker.graphic.CardProductCard
 import it.polito.progettolocker.graphic.CardWarning
 import it.polito.progettolocker.graphic.FooterDoubleBlack
@@ -80,7 +87,9 @@ fun Carrello(mainActivity: MainActivity, navController: NavController){
             HeaderX(text = "CARRELLO", navController = navController, onClickDestination = "Customer")
         },
         bottomBar = {
-            FooterDoubleBlack(price = price.value, navController = navController)
+            if(price.value != 0.0){
+                FooterDoubleBlack(price = price.value, navController = navController)
+            }
         },
     ) { innerPadding ->
         Column (
@@ -96,27 +105,67 @@ fun Carrello(mainActivity: MainActivity, navController: NavController){
 
                 is DataState.Success -> {
 
-                                Row {
-                                    LazyColumn{
-                                        items(result.data as List<Article>) { article ->
-                                                CardProductCard(
-                                                    mainActivity=mainActivity,
-                                                    navController = navController,
-                                                    article=article,
-                                                    textProduct = article.name!!,
-                                                    price = article.price!!.toFloat(),
-                                                    quantity = article.quantity!!.toInt(),
-                                                    image=article.image!!.toString(),
-                                                )
-                                                Divider(color = Color.LightGray, thickness = 1.dp)
-
-                                            }
+                    if(price.value == 0.0){
+                        Column(
+                            modifier = Modifier
+                                .padding(innerPadding)
+                        ) {
+                            Column(
+                                verticalArrangement = Arrangement.spacedBy(16.dp),
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                modifier = Modifier.padding(top = 70.dp)
+                            ) {
+                                Row(
+                                    modifier = Modifier.padding(start = 35.dp, end = 35.dp),
+                                    horizontalArrangement = Arrangement.Center
+                                ) {
+                                    Text(
+                                        text = "CARRELLO VUOTO",
+                                        fontWeight = FontWeight.ExtraBold,
+                                        fontSize = 18.sp,
+                                        color = Color(0xFF000000)
+                                    )
+                                }
+                                Column(
+                                    horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier
+                                        .padding(top = 30.dp)
+                                        .fillMaxWidth()
+                                ) {
+                                    Text(
+                                        text = "INIZIA A FARE SHOPPING",
+                                        fontSize = 15.sp,
+                                        color = Color(0xFF000000)
+                                    )
+                                }
+                                Row(modifier = Modifier.padding(top = 20.dp)) {
+                                    Buttons(
+                                        text = "CATALOGO",
+                                        onClickHandler = {
+                                            navController.navigate("Catalogo")
                                         }
-
-
-
-                                    }
+                                    )
+                                }
+                            }
+                        }
+                    } else {
+                        Row {
+                            LazyColumn{
+                                items(result.data as List<Article>) { article ->
+                                    CardProductCard(
+                                        mainActivity=mainActivity,
+                                        navController = navController,
+                                        article=article,
+                                        textProduct = article.name!!,
+                                        price = article.price!!.toFloat(),
+                                        quantity = article.quantity!!.toInt(),
+                                        image=article.image!!.toString(),
+                                    )
+                                    Divider(color = Color.LightGray, thickness = 1.dp)
+                                }
+                            }
+                        }
                     }
+                }
 
                 is DataState.Failure -> {
                     CardWarning(
