038226fee9c056ba9d3e9f360ddbd36e1f70a206
GiadaFerreri <s314885@studenti.polito.it>
2024-02-23 16:21:33 +0100

    nessuna spedizione testo

--- app/src/main/java/it/polito/progettolocker/views/delivery/DaEffettuare.kt
+++ app/src/main/java/it/polito/progettolocker/views/delivery/DaEffettuare.kt
@@ -3,6 +3,7 @@ package it.polito.progettolocker.views.delivery
 import android.content.ContentValues
 import android.util.Log
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,10 +11,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableDoubleStateOf
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -30,6 +37,8 @@ import it.polito.progettolocker.graphic.HeaderDouble
 fun DaEffettuare(mainActivity: MainActivity, navController: NavController){
 
     val tempList = mutableListOf<Shipping>()
+    var available=0
+    var spedizioni=false
 
     mainActivity.viewModel.db.child("Shipping")
         .addValueEventListener(object: ValueEventListener {
@@ -70,27 +79,49 @@ fun DaEffettuare(mainActivity: MainActivity, navController: NavController){
                 }
 
                 is DataState.Success -> {
-                    Row {
-                        LazyColumn(modifier= Modifier.padding(bottom=30.dp)) {
-                            items(result.data as List<Shipping>) { shipping ->
-                                Row {
-                                    if (shipping.state == States.PENDING) {
-                                        CardOrder(
-                                            shipping = shipping,
-                                            orderNumber = shipping.shippingId!!.toString(),
-                                            description = "CONSEGNA AL LOCKER LINGOTTO",
-                                            leftButtonText = "PRESA IN CARICO",
-                                            mainActivity = mainActivity,
-                                            navController = navController,
-                                            onClickDestination = "InCorso",
-                                            toHandle = true
-                                        )
-                                    }
 
+
+                    LazyColumn(modifier = Modifier.padding(bottom = 30.dp)) {
+
+                        items(result.data as List<Shipping>) { shipping ->
+                            Row {
+                                if (shipping.state == States.PENDING) {
+                                    CardOrder(
+                                        shipping = shipping,
+                                        orderNumber = shipping.shippingId!!.toString(),
+                                        description = "CONSEGNA AL LOCKER LINGOTTO",
+                                        leftButtonText = "PRESA IN CARICO",
+                                        mainActivity = mainActivity,
+                                        navController = navController,
+                                        onClickDestination = "InCorso",
+                                        toHandle = true
+                                    )
+                                    spedizioni = true
                                 }
+
+
                             }
+
+                        }
+
+                    }
+                    if (!spedizioni) {
+                        Row(
+                            modifier = Modifier.padding(start = 100.dp, end = 35.dp, top=70.dp),
+                            horizontalArrangement = Arrangement.Center
+                        ) {
+                            Text(
+                                text = "NESSUNA SPEDIZIONE DA EFFETTUARE",
+                                fontWeight = FontWeight.ExtraBold,
+                                fontSize = 15.sp,
+                                color = Color(0xFF000000)
+                            )
+
+
                         }
                     }
+
+
                 }
 
                 is DataState.Failure -> {
--- app/src/main/java/it/polito/progettolocker/views/delivery/InCorso.kt
+++ app/src/main/java/it/polito/progettolocker/views/delivery/InCorso.kt
@@ -3,6 +3,7 @@ package it.polito.progettolocker.views.delivery
 import android.content.ContentValues
 import android.util.Log
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,6 +13,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -19,6 +21,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -34,6 +37,7 @@ import it.polito.progettolocker.graphic.HeaderDouble
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun InCorso(mainActivity: MainActivity, navController: NavController){
+    var spedizioni=false
 
     val tempList = mutableListOf<Shipping>()
 
@@ -101,9 +105,26 @@ fun InCorso(mainActivity: MainActivity, navController: NavController){
                                             navController = navController,
                                             onClickDestination = "Locker"
                                         )
+                                        spedizioni = true
                                     }
 
                                 }
+                            }
+                        }
+                        if (!spedizioni) {
+                            Row(
+                                modifier = Modifier.padding(start = 60.dp, end = 35.dp, top=70.dp),
+                                horizontalArrangement = Arrangement.Center
+                            ) {
+                                Text(
+                                    text = "NESSUNA SPEDIZIONE IN CORSO",
+                                    fontWeight = FontWeight.ExtraBold,
+                                    fontSize = 15.sp,
+                                    color = Color(0xFF000000)
+
+                                )
+
+
                             }
                         }
                     }
