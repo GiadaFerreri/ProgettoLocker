0a1dc0492636430997c101547ecb273f6daf6c2f
carolinaicardi1 <s315064@studenti.polito.it>
2024-02-26 18:32:22 +0100

    Ordinamento spedizioni

--- app/src/main/java/it/polito/progettolocker/views/delivery/DaEffettuare.kt
+++ app/src/main/java/it/polito/progettolocker/views/delivery/DaEffettuare.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -36,9 +37,10 @@ import it.polito.progettolocker.graphic.HeaderDouble
 @Composable
 fun DaEffettuare(mainActivity: MainActivity, navController: NavController){
 
-    val tempList = mutableListOf<Shipping>()
+    val sortedShippingList = mutableListOf<Shipping>()
     var available=0
     var spedizioni=false
+
 
     mainActivity.viewModel.db.child("Shipping")
         .addValueEventListener(object: ValueEventListener {
@@ -47,15 +49,21 @@ fun DaEffettuare(mainActivity: MainActivity, navController: NavController){
 
                 for(snapshot in snapshot.children){
                     val shipping = snapshot.getValue(Shipping::class.java)
-                    tempList.add(shipping!!)
+                    sortedShippingList.add(shipping!!)
                 }
-                mainActivity.viewModel.shippingState.value = DataState.Success(tempList)
+                /*val sortedCountShippingList = sortedShippingList
+                    .mapNotNull { it }
+                    .sortedByDescending { it.countShipping }*/
+
+                mainActivity.viewModel.shippingState.value = DataState.Success(sortedShippingList)
             }
 
             override fun onCancelled(error: DatabaseError) {
                 Log.w(ContentValues.TAG, "Failed to read value.", error.toException())
             }
         })
+
+
 
     Column{
         Row{
@@ -80,31 +88,28 @@ fun DaEffettuare(mainActivity: MainActivity, navController: NavController){
 
                 is DataState.Success -> {
 
-
                     LazyColumn(modifier = Modifier.padding(bottom = 30.dp)) {
-
                         items(result.data as List<Shipping>) { shipping ->
                             Row {
-                                if (shipping.state == States.PENDING) {
-                                    CardOrder(
-                                        shipping = shipping,
-                                        orderNumber = shipping.shippingId!!.toString(),
-                                        description = "CONSEGNA AL LOCKER LINGOTTO",
-                                        leftButtonText = "PRESA IN CARICO",
-                                        mainActivity = mainActivity,
-                                        navController = navController,
-                                        onClickDestination = "InCorso",
-                                        toHandle = true
-                                    )
-                                    spedizioni = true
+                               if(shipping.state== States.PENDING){
+                                   CardOrder(
+                                       shipping = shipping,
+                                       orderNumber = shipping.shippingId!!.toString(),
+                                       description = "CONSEGNA AL LOCKER LINGOTTO",
+                                       leftButtonText = "PRESA IN CARICO",
+                                       mainActivity = mainActivity,
+                                       navController = navController,
+                                       onClickDestination = "InCorso",
+                                       toHandle = true
+                                   )
+                                   spedizioni = true
+                               }
                                 }
-
 
                             }
 
                         }
 
-                    }
                     if (!spedizioni) {
                         Row(
                             modifier = Modifier.padding(start = 40.dp, top=350.dp),
